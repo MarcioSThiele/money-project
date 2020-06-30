307fd3d4e2acd6f98f2ea473d65448c40fc752a5
MarcioSThiele <marcio.sthiele@gmail.com>
2020-06-30 15:45:09 -0300

    Adding initial files

---
+++ src/main/java/com/thielem/moneyproject/MoneyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.thielem.moneyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyProjectApplication.class, args);
+	}
+
+}
